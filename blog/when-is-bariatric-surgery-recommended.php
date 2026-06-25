5b489363c3040afd3ce8f19b258f0354c41788d7
anshrealestate <anshrealestate@gmail.com>
2026-06-25 16:27:12 +0530

    Update when is bariatric surgery recommended blog post thumbnail image and references

--- blog/when-is-bariatric-surgery-recommended.php
+++ blog/when-is-bariatric-surgery-recommended.php
@@ -35,7 +35,7 @@ include __DIR__ . '/../includes/header.php';
 
             <!-- Featured Image -->
             <figure class="mb-10">
-                <img src="/blog/media/images/Bariatric Surgery.webp" alt="When is Bariatric Surgery Recommended?" class="w-full rounded-2xl shadow-lg">
+                <img src="/blog/media/images/Bariatric-Surgery-Recommended.jpg" alt="When is Bariatric Surgery Recommended?" class="w-full rounded-2xl shadow-lg">
             </figure>
 
             <!-- Article Content -->
